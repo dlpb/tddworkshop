2369185bce256feba4b505e5866c2ab15a55f0f7
Dan Brown <dbrown7@ebay.com>
2016-01-26 17:42:29 +0000

    adding implementation for two different properties

--- src/main/scala/com/tddworkshop/config/Config.scala
+++ src/main/scala/com/tddworkshop/config/Config.scala
@@ -1,5 +1,9 @@
 package com.tddworkshop.config
 
 class Config {
-  def valueOf(s: String) = "present"
+  def valueOf(s: String) = s match {
+    case "present_value" => "present"
+    case "value_1" => "value1"
+    case "value_2" => "value2"
+  }
 }
