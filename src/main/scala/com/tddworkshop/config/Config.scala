ea77ebcf8d6b7a9dc8209679371591f90f201219
Dan Brown <dbrown7@ebay.com>
2016-01-26 17:40:31 +0000

    getting a property when the value is present

---
+++ src/main/scala/com/tddworkshop/config/Config.scala
@@ -0,0 +1,5 @@
+package com.tddworkshop.config
+
+class Config {
+  def valueOf(s: String) = "present"
+}
--- src/test/scala/com/tddworkshop/config/ServiceConfigTest.scala
+++ src/test/scala/com/tddworkshop/config/ServiceConfigTest.scala
@@ -1,7 +1,13 @@
 package com.tddworkshop.config
 
-import org.scalatest.FlatSpec
+import org.scalatest.{Matchers, FlatSpec}
 
-class ServiceConfigTest extends FlatSpec {
-  
+class ServiceConfigTest extends FlatSpec with Matchers {
+
+  "The config loader" should "give a value when geting a property whose value is present" in {
+    val config = new Config()
+    config.valueOf("present_value") should be("present")
+  }
+
+
 }
