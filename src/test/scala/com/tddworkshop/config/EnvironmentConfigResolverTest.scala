545649dc8f57aec1e3c0f7810d0dbb2036215ba0
Dan Brown <dbrown7@ebay.com>
2016-01-27 17:57:21 +0000

    adding start of test for environment resolver

---
+++ src/test/scala/com/tddworkshop/config/EnvironmentConfigResolverTest.scala
@@ -0,0 +1,12 @@
+package com.tddworkshop.config
+
+import org.scalatest.{Matchers, FlatSpec}
+
+class EnvironmentConfigResolverTest extends FlatSpec with Matchers {
+
+  "Environment resolver" should "load config for a specified environment from a folder of the same name" in {
+    val config = EnvironmentConfigResolver.load("qa")
+    config.getValue("source") should be("file")
+    config.getValue("environment") should be("qa")
+  }
+}
